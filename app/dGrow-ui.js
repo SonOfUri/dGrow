7bc034be1ff4580ba2e617caab6ab6ec09c6bda5
Alo Timothy <59998792+SonOfUri@users.noreply.github.com>
2023-03-27 01:53:31 +0100

    Final Upoload

--- app/dGrow-ui.js
+++ app/dGrow-ui.js
@@ -31,4 +31,47 @@ function appDisplay2(){
 
     navStake.classList.remove("utility-class_1");
     navSwap.classList.add("utility-class_1");
+
+}
+
+
+const hideClaimBtn = document.getElementById('toggleClaim');
+
+hideClaimBtn.addEventListener("click", hideProps);
+
+function hideProps(){
+    var prop1 = document.getElementById('claimVar1');
+    var prop2 = document.getElementById('claimVar2');
+
+    prop1.classList.remove("utility-class_2");
+    prop2.classList.remove("utility-class_2");
+
+    hideClaimBtn.style.transform = "rotate(180deg)";
+    hideClaimBtn.style.transition = "0.5s";
+
+}
+
+const stakeBtn = document.getElementById('stakeBtn');
+const unstakeBtn = document.getElementById('unstakeBtn');
+
+
+stakeBtn.addEventListener("click", appDisplay3);
+function appDisplay3(){
+    document.getElementById('unstakeBoard').classList.add("utility-class_2");
+    document.getElementById('stakeBoard').classList.remove("utility-class_2");
+
+    unstakeBtn.classList.remove("utility-class_1");
+    stakeBtn.classList.add("utility-class_1");
+
+}
+
+
+unstakeBtn.addEventListener("click", appDisplay4);
+function appDisplay4(){
+    document.getElementById('stakeBoard').classList.add("utility-class_2");
+    document.getElementById('unstakeBoard').classList.remove("utility-class_2");
+
+    stakeBtn.classList.remove("utility-class_1");
+    unstakeBtn.classList.add("utility-class_1");
+
 }
